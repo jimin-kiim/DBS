8ce1570de7b36f91403cd90d7e40cd2532c1ffec
Jimin Kim <0305jimin@gmail.com>
2022-12-02 19:01:02 +0900

    Feat: executing queries dynamically

--- DeliveryService/src/DeliveryService.java
+++ DeliveryService/src/DeliveryService.java
@@ -10,19 +10,21 @@ public class DeliveryService {
 static final String DB_URL = "jdbc:mysql://localhost/DeliveryService?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone= UTC";
 static final String USER = "root"; 
 static final String PASS = "wlalsdlqslek!"; 
+static final Scanner sc = new Scanner(System.in);
 
 public void execute() {
 	boolean flag = true;
-	Scanner sc = new Scanner(System.in);
-	
+
 	while(flag) {
 		System.out.println("수행할 기능을 선택해주세요. ");
+		System.out.println("");
 		System.out.println("1. 1인 메뉴 주문 가능한 음식점 검색 ");
 		System.out.println("2. 작성한 리뷰 검색 ");
 		System.out.println("3. 종료 ");
+		System.out.println(">>");
 		
-		int user_input = sc.nextInt(); // 배열 원소 개수
-	    sc.nextLine(); // 버퍼 비우기
+		int user_input = sc.nextInt(); 
+	    sc.nextLine(); 
 		
 	    switch (user_input) {	
 		case 1:	
@@ -37,29 +39,39 @@ public void execute() {
 		default:
 			System.out.println("잘못된 입력입니다.");
 		}
+	    System.out.println("");
 	}
+	System.out.println("종료");
 }
 
 
 public void searchRestaurants(){
-	System.out.println("searchRestaurants.");
 	Connection conn = null;
 	Statement stmt = null;
 	ResultSet rs = null;
 	PreparedStatement pstmt = null;
 	try {
+		System.out.println("");
+		System.out.println("음식점 분류를 선택해주세요. ");
+		System.out.println("");
+		System.out.println("0. 한식 ");
+		System.out.println("1. 양식 ");
+		System.out.println("2. 중식 ");
+		System.out.println("3. 일식 ");
+		System.out.println(">>");
+		
+		int user_input = sc.nextInt(); 
+	    sc.nextLine(); 
+	    
 		conn = DriverManager.getConnection(DB_URL,USER,PASS);
 		stmt = conn.createStatement();
 		
 		System.out.println("====================================");
 		String sql = "select * "
 				+ "from restaurant "
-				+ "where is_one_person_set_available in "
-					+ "(select is_one_person_set_available "
-					+ "from restaurant "
-					+ "where category = ?)";
+				+ "where is_one_person_set_available = 1 and category = ?";
 		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, 0);
+		pstmt.setInt(1, user_input);
 		rs = pstmt.executeQuery();
 		
 		while(rs.next()) {
@@ -71,6 +83,7 @@ public void searchRestaurants(){
 					" 최소 주문 금액: " + rs.getInt("min_order_amount") +"원"
 		);
 		}
+		System.out.println("====================================");
 	}catch(SQLException e) {
 		System.out.println("SQLException : "+e);
 	}finally {
@@ -78,7 +91,6 @@ public void searchRestaurants(){
 			rs.close();
 			stmt.close();
 			pstmt.close();
-			
 			conn.close();
 		}catch(SQLException e) {
 			e.printStackTrace();
@@ -87,12 +99,16 @@ public void searchRestaurants(){
 }
 
 public void searchReviews(){
-	System.out.println("searchReviews");
 	Connection conn = null;
 	Statement stmt = null;
 	ResultSet rs = null;
 	PreparedStatement pstmt = null;
 	try {
+		System.out.println("");
+		System.out.println("작성자 고객의 이름을 입력해주세요. >>");
+		String user_input = sc.nextLine(); 
+	    sc.nextLine(); 
+	    
 		conn = DriverManager.getConnection(DB_URL,USER,PASS);
 		stmt = conn.createStatement();
 		
@@ -105,18 +121,18 @@ public void searchReviews(){
 					+ "where name = ?)";
 		
 		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, "고객1");
+		pstmt.setString(1, user_input);
 		rs = pstmt.executeQuery();
 		
 		while(rs.next()) {
 			System.out.println(
-//					"이름: "+ rs.getString("name") + 
 					" 작성시간: " + rs.getString("created_at") +
 					" 별점: " + rs.getInt("rating") +
 					" 내용: " + rs.getString("contents") +
 					" 첨부 사진: " + rs.getString("image_url")
 		);
 		}
+		System.out.println("====================================");
 	}catch(SQLException e) {
 		System.out.println("SQLException : "+e);
 	}finally {
